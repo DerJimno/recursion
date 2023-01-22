2415c602663ab9b941c544654d9fe0a78b3c7cdb
Jimno <menaoir.jimmy@gmail.com>
2023-01-22 12:20:46 +0100

    done using iteration

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 # Assignment 1
-def fib(n)
-  return 0 if n.zero?
-  n == 1 ? 1 : fib(n - 2) + fib(n - 1)
+def fibs(num, arr = [])
+  first_num = 0
+  sec_num = 1
+  0..num.times do
+    arr << first_num
+    first_num, sec_num = sec_num, first_num + sec_num
+  end
+  arr 
 end
 
-# p fib(7)
+p fibs(8)
