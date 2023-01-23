4241bad7843b933a14a373320df0bbc17c3baa2f
Jimno <menaoir.jimmy@gmail.com>
2023-01-23 12:36:27 +0100

    fixed for 0 & 1 to work as arguments

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
-# Assignment 1
-# Part 1:
+# Assignment 1 (Method to return an array of Fabonacci sequence)
+# Part 1: using Iterators/Loops
 def fibs(num)
   fib = [0,1]
+  return [0] if num == 0
+  return [0,1] if num == 1
   until num == 2
     fib << fib[-1] + fib[-2]
     num -= 1
@@ -10,13 +12,15 @@ def fibs(num)
 end
 p fibs(5) # => [0, 1, 1, 2, 3]
 
-# Part 2:
+# Part 2: using Recursion
 @fib = [0,1]
 def fibs_rec(num)
+  return [0] if num == 0
+  return [0,1] if num == 1
   return @fib if num == 2
   @fib << @fib[-1] + @fib[-2]
   fibs_rec(num - 1)
 end
 p fibs_rec(5)  # => [0, 1, 1, 2, 3]
 
-# Assignment 2 
+# Assignment 2 ()
