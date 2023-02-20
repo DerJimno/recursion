a703db16baa4989e66a1f7041388ee72afd676eb
Jimno <menaoir.jimmy@gmail.com>
2023-02-20 11:44:52 +0100

    add brevity...

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
 # Assignment 1 (Method to return an array of Fabonacci sequence)
-# Part 1: using Iterators/Loops
+# Part 1: using Loops
 def fibs(num)
   fib = [0,1]
-  return [0] if num == 0
-  return [0,1] if num == 1
+  return fib[0..num - 1] if num <= 2
   until num == 2
     fib << fib[-1] + fib[-2]
     num -= 1
@@ -12,14 +11,12 @@ def fibs(num)
 end
 p fibs(5) # => [0, 1, 1, 2, 3]
 
-# Part 2: using Recursion
+# Part 2: using Recursion 
 @fib = [0,1]
 def fibs_rec(num)
-  return [0] if num == 0
-  return [0,1] if num == 1
-  return @fib if num == 2
-  @fib << @fib[-1] + @fib[-2]
+  return @fib[0..num - 1] if num <= 2
   fibs_rec(num - 1)
+  @fib << @fib[-1] + @fib[-2]
 end
 p fibs_rec(5)  # => [0, 1, 1, 2, 3]
 
