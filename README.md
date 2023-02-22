09c8a07f6076cc419534faddf79241b2c1624b62
Jimno <menaoir.jimmy@gmail.com>
2023-02-22 15:54:42 +0100

    update fibs, added merge_sort, update README.md

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 Project: Recursion from theodinproject.com.
 
-# Assignment 1 (Method that returns an array of Fabonacci sequence till a number given RECURSIVELY!!!!)
+# Assignment 1 (Method that returns an array of Fabonacci sequence RECURSIVELY!!!!)
 
 It was kinda weird to get the fabonacci of a number recursively and
 at the same time do another call of the method to get the next 
 one (recursively) untill we have the array, that seemed to be wrong,
 like an infinite loop, I can't see how one can manage to get it that way...
-Instead I just used the brute-force way to have the fabonaccis progressively
-with the help of recursion in few lines!
+Instead I just got fabonaccis progressively with the help of recursion in fewer lines!
+
+# Assignment 2 (Method to sort an Array using recursion)
+
+Ooof this one wasn't that simple!
--- main.rb
+++ main.rb
@@ -3,15 +3,12 @@
 def fibs(num)
   fib = [0,1]
   return fib[0..num - 1] if num <= 2
-  until num == 2
-    fib << fib[-1] + fib[-2]
-    num -= 1
-  end
+  (num-2).times {fib << fib[-1] + fib[-2]}
   fib
 end
-p fibs(5) # => [0, 1, 1, 2, 3]
+p fibs(5) # =>[0, 1, 1, 2, 3]
 
-# Part 2: using Recursion 
+# Part 2: using Recursion
 @fib = [0,1]
 def fibs_rec(num)
   return @fib[0..num - 1] if num <= 2
@@ -20,4 +17,16 @@ def fibs_rec(num)
 end
 p fibs_rec(5)  # => [0, 1, 1, 2, 3]
 
-# Assignment 2 ()
+# Assignment 2 (Merge sort - A sorting Algorithm that uses Recursion{devide&conquer})
+def merge_sort(array, lowest = [])
+  return array if array.length < 2 # Base-case
+
+  left = merge_sort(array.shift(array.length/2)) # Left-part of array on each recursion
+  right = merge_sort(array) # Right-part...
+
+  until left.empty? || right.empty?
+    left.first <= right.first ? lowest << left.shift : lowest << right.shift # get lowest
+  end
+  lowest + left + right
+end
+p merge_sort([100, 2, 90, 1, 9, 12 ,3, 7, 5]) # =>[1, 2, 3, 5, 7, 9, 12, 90, 100]
